1879c0426544432d867f235cfd96036c5013184f
coladavid11 <liu.jiun.le@student.mmu.edu.my>
2026-06-16 03:26:22 +0800

    up

--- user/myorder_detail.php
+++ user/myorder_detail.php
@@ -1155,7 +1155,6 @@ $invoiceData = json_encode([
 
     <?php include('includes/footer.php'); ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
